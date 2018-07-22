bb5583ff30c9d948e70d537d9afa00d46bd70e65
Rishi <rbommasani@mozilla.com>
2018-07-22 13:59:12 -0700

    Copy0

--- onmt/modules/copy_generator.py
+++ onmt/modules/copy_generator.py
@@ -93,7 +93,7 @@ class CopyGenerator(nn.Module):
         logits[:, self.tgt_dict.stoi[inputters.PAD_WORD]] = -float('inf')
         prob = self.softmax(logits)
 
-        copy_temperature = 2.0
+        copy_temperature = 0.0
         adaptive = True
         if adaptive:
             # Probability of copying p(z=1) batch.
