3aa0a15f455c395823bd5cfeb3d5592f0c6944e8
Rishi <rbommasani@mozilla.com>
2018-07-26 16:51:01 -0700

    Job 828, 256,512,2,250k

--- onmt/opts.py
+++ onmt/opts.py
@@ -203,7 +203,7 @@ def preprocess_opts(parser):
                        help="Random seed")
 
     group = parser.add_argument_group('Logging')
-    group.add_argument('-report_every', type=int, default=1000,
+    group.add_argument('-report_every', type=int, default=10000,
                        help="Report status every this many sentences")
     group.add_argument('-log_file', type=str, default="",
                        help="Output logs to a file under this path.")
@@ -366,10 +366,10 @@ def train_opts(parser):
                        this much if (i) perplexity does not decrease on the
                        validation set or (ii) steps have gone past
                        start_decay_steps""")
-    group.add_argument('-start_decay_steps', type=int, default=50000,
+    group.add_argument('-start_decay_steps', type=int, default=150000,
                        help="""Start decaying every decay_steps after
                        start_decay_steps""")
-    group.add_argument('-decay_steps', type=int, default=10000,
+    group.add_argument('-decay_steps', type=int, default=25000,
                        help="""Decay every decay_steps""")
 
     group.add_argument('-decay_method', type=str, default="",
